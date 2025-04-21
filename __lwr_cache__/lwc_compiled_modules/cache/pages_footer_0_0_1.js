ed4d05eb0fe73fa91c81eb317bccc6df04da4134
abhipotdar123 <abhinavpotdar1@gmail.com>
2025-04-21 19:38:06 +0530

    delete-node_modules_cache

--- __lwr_cache__/lwc_compiled_modules/cache/pages_footer_0_0_1.js
+++
@@ -1,9 +0,0 @@
-import _tmpl from "./footer.html";
-import { registerComponent as _registerComponent, LightningElement } from "lwc";
-class Footer extends LightningElement {
-  /*LWC compiler v2.50.0*/
-}
-export default _registerComponent(Footer, {
-  tmpl: _tmpl,
-  sel: ""
-});
